0fe079ada6edcf2e984b4f732e16707e6b3c4e9b
Reema <reema.sh2211@gmail.com>
2021-09-10 17:46:07 +0300

    first route /movies

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import *
+from auth import AuthError, requires_auth
+
 
 def create_app(test_config=None):
 
@@ -24,6 +26,18 @@ def create_app(test_config=None):
         if excited == 'true':
             greeting = greeting + "!!!!!"
         return  greeting
+    # *********************************************
+    #           Movies                            #
+    # *********************************************
+    @app.route('/movies', methods=['GET'])
+    @requires_auth('get:movies')
+    def view_movies(payload):
+        movies = Movies.query.all()
+        if len(movies) == 0:
+            abort(404)
+
+        return jsonify({'success': True, 'actors': [movie.format()
+                       for movie in movies]}), 200
     return app
 
 app = create_app()
