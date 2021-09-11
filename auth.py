546acef42b4fce910c2aaa21872a75f53b8f5cf7
Reema <reema.sh2211@gmail.com>
2021-09-11 09:13:13 +0300

    Fixing error mus

--- auth.py
+++ auth.py
@@ -4,6 +4,8 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
+from werkzeug.exceptions import abort
+
 
 AUTH0_DOMAIN = 'dev-xudjg1cu.us.auth0.com'
 ALGORITHMS = ['RS256']
@@ -16,7 +18,6 @@ AuthError Exception
 A standardized way to communicate auth failure modes
 '''
 
-
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
@@ -26,7 +27,7 @@ class AuthError(Exception):
 # Auth Header
 
 '''
-@[DONE] implement get_token_auth_header() method
+Implement get_token_auth_header() method
     it should attempt to get the header from the request
         it should raise an AuthError if no header is present
     it should attempt to split bearer and the token
@@ -34,73 +35,50 @@ class AuthError(Exception):
     return the token part of the header
 '''
 
-
 def get_token_auth_header():
-    """Obtains the Access Token from the Authorization Header
-    """
-    auth = request.headers.get('Authorization', None)
-    if not auth:
-        raise AuthError({
-            'code': 'authorization_header_missing',
-            'description': 'Authorization header is expected.'
-        }, 401)
-
-    parts = auth.split()
-    if parts[0].lower() != 'bearer':
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization header must start with "Bearer".'
-        }, 401)
-
-    elif len(parts) == 1:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Token not found.'
-        }, 401)
-
-    elif len(parts) > 2:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization header must be bearer token.'
-        }, 401)
-
-    token = parts[1]
-    return token
-
+    if 'Authorization' not in request.headers:
+        abort(401)
+    token = request.headers["Authorization"]
+    
+    auth= token.split(' ')
+    
+    print("Token: " + token + "," + str(len(auth)), flush=True)
+    if len(auth) != 2:
+        abort(401)
+    elif auth[0].lower() != 'bearer':
+        abort(401)
+        
+    return auth[1]
 
 '''
-@[DONE] implement check_permissions(permission, payload) method
+@DONE implement check_permissions(permission, payload) method
     @INPUTS
         permission: string permission (i.e. 'post:drink')
         payload: decoded jwt payload
     it should raise an AuthError if permissions are not included in the payload
         !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in
-    the payload permissions array
+    it should raise an AuthError if the requested permission string is not in the payload permissions array
     return true otherwise
 '''
-
-
 def check_permissions(permission, payload):
-    #raise an AuthError if permissions are not included in the payload
+    """
+        Check if permission exicts in payload
+    """
     if 'permissions' not in payload:
         raise AuthError({
             'code': 'invalid_claims',
             'description': 'Permissions not included in JWT.'
         }, 400)
-    # raise an AuthError if the requested permission string is not in
-    # the payload permissions array
+
     if permission not in payload['permissions']:
         raise AuthError({
             'code': 'unauthorized',
             'description': 'Permission not found.'
-        }, 403)
-    #return true otherwise
+        }, 401)
     return True
 
-
 '''
-@[DONE] implement verify_decode_jwt(token) method
+@DONE implement verify_decode_jwt(token) method
     @INPUTS
         token: a json web token (string)
     it should be an Auth0 token with key id (kid)
@@ -108,13 +86,14 @@ def check_permissions(permission, payload):
     it should decode the payload from the token
     it should validate the claims
     return the decoded payload
+    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
-
-
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
+    print("unverified header:")
+    print(unverified_header, flush=True)
     rsa_key = {}
     if 'kid' not in unverified_header:
         raise AuthError({
@@ -132,6 +111,7 @@ def verify_decode_jwt(token):
                 'e': key['e']
             }
     if rsa_key:
+        print(rsa_key, flush=True)
         try:
             payload = jwt.decode(
                 token,
@@ -140,6 +120,7 @@ def verify_decode_jwt(token):
                 audience=API_AUDIENCE,
                 issuer='https://' + AUTH0_DOMAIN + '/'
             )
+
             return payload
 
         except jwt.ExpiredSignatureError:
@@ -151,8 +132,7 @@ def verify_decode_jwt(token):
         except jwt.JWTClaimsError:
             raise AuthError({
                 'code': 'invalid_claims',
-                'description': 'Incorrect claims.'
-                + 'Please, check the audience and issuer.'
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
             }, 401)
         except Exception:
             raise AuthError({
@@ -166,25 +146,21 @@ def verify_decode_jwt(token):
 
 
 '''
-@[DONE] implement @requires_auth(permission) decorator method
+@DONE implement @requires_auth(permission) decorator method
     @INPUTS
         permission: string permission (i.e. 'post:drink')
+    it should use the get_token_auth_header method to get the token
+    it should use the verify_decode_jwt method to decode the jwt
+    it should use the check_permissions method validate claims and check the requested permission
+    return the decorator which passes the decoded payload to the decorated method
 '''
-
-
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            #use the get_token_auth_header method to get the token
             token = get_token_auth_header()
-            #use the verify_decode_jwt method to decode the jwt
             payload = verify_decode_jwt(token)
-            # check_permissions method validate claims and check the
-            # requested permission
             check_permissions(permission, payload)
-            #return the decorator which passes the decoded payload to the
-            #decorated method
             return f(payload, *args, **kwargs)
 
         return wrapper
